58bf263af1326c8079fbec24c8bc6d88b69df3ee
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-02-15 21:23:34 -0400

    get and post cars in db

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from flask import Flask, make_response, request, jsonify
-from bd import Cars
+from db import myDb
 
 app = Flask(__name__) # to use the name of current file
 app.config['JSON_SORT_KEYS'] = False # to set false the alphabetic organization
+myCursor = myDb.cursor()
 
 @app.route('/carros', methods=['GET'])
 def getCar():
+    myCursor.execute('SELECT * FROM cars')
+    myCars = myCursor.fetchall()
+    cars = list()
+    for eachCar in myCars:
+        cars.append(
+            {
+                'id': eachCar[0],
+                'brand': eachCar[1],
+                'model': eachCar[2],
+                'year': eachCar[3]
+            }
+        )
+
     return make_response(
-            jsonify(Cars)
+            jsonify(cars)
     )
 
 @app.route('/carros', methods=['POST'])
 def createCar():
     car = request.json
-    Cars.append(car)
+    sql = f"INSERT INTO cars (brand, model, year) VALUES ('{car['brand']}', '{car['model']}', {car['year']})"
+    myCursor.execute(sql)
+    myDb.commit()
     return make_response(
         jsonify(
             message="Success to add car!",
